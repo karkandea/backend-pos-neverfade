7ce5c317146630d85f76077b4ef389648369686e
Arkan <98953892+karkandea@users.noreply.github.com>
2026-09-02 14:31:06 +0700

    feat: configure shared POS sessions

---
+++ NeverfadePos.Api/Data/Configurations/SharedPosSessionConfiguration.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NeverfadePos.Api.Entities;
+
+namespace NeverfadePos.Api.Data.Configurations;
+
+public sealed class SharedPosSessionConfiguration : IEntityTypeConfiguration<SharedPosSession>
+{
+    public void Configure(EntityTypeBuilder<SharedPosSession> builder)
+    {
+        builder.ToTable("shared_pos_sessions");
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => x.TokenHash).IsUnique();
+        builder.HasIndex(x => new { x.TenantId, x.DeviceId, x.ExpiresAtUtc });
+        builder.Property(x => x.TokenHash).HasMaxLength(64).IsRequired();
+
+        builder.HasOne(x => x.Tenant)
+            .WithMany(x => x.SharedPosSessions)
+            .HasForeignKey(x => x.TenantId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Device)
+            .WithMany(x => x.Sessions)
+            .HasForeignKey(x => x.DeviceId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Karyawan)
+            .WithMany(x => x.SharedPosSessions)
+            .HasForeignKey(x => x.KaryawanId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
