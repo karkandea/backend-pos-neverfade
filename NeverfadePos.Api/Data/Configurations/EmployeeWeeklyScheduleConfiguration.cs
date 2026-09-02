3e8b709b99dfd269c8516b9b4a1e1d222870ec25
Arkan <98953892+karkandea@users.noreply.github.com>
2026-09-02 14:30:26 +0700

    feat: configure recurring employee schedule

---
+++ NeverfadePos.Api/Data/Configurations/EmployeeWeeklyScheduleConfiguration.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NeverfadePos.Api.Entities;
+
+namespace NeverfadePos.Api.Data.Configurations;
+
+public sealed class EmployeeWeeklyScheduleConfiguration : IEntityTypeConfiguration<EmployeeWeeklySchedule>
+{
+    public void Configure(EntityTypeBuilder<EmployeeWeeklySchedule> builder)
+    {
+        builder.ToTable("employee_weekly_schedules", table =>
+            table.HasCheckConstraint("CK_employee_weekly_schedules_DayOfWeek", "\"DayOfWeek\" BETWEEN 0 AND 6"));
+
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => new { x.TenantId, x.KaryawanId, x.DayOfWeek }).IsUnique();
+
+        builder.HasOne(x => x.Tenant)
+            .WithMany(x => x.EmployeeWeeklySchedules)
+            .HasForeignKey(x => x.TenantId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Karyawan)
+            .WithMany(x => x.WeeklySchedules)
+            .HasForeignKey(x => x.KaryawanId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
