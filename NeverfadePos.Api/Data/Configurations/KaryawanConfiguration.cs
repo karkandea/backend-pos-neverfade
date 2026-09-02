4c12f589ebf6ecaed7640998e7ea3752ba2c98dc
Arkan <98953892+karkandea@users.noreply.github.com>
2026-09-02 10:22:07 +0700

    feat: configure employee POS link and PIN

--- NeverfadePos.Api/Data/Configurations/KaryawanConfiguration.cs
+++ NeverfadePos.Api/Data/Configurations/KaryawanConfiguration.cs
@@ -11,8 +11,10 @@ public class KaryawanConfiguration : IEntityTypeConfiguration<Karyawan>
         builder.ToTable("karyawans");
 
         builder.HasKey(x => x.Id);
-
         builder.HasIndex(x => x.TenantId);
+        builder.HasIndex(x => new { x.TenantId, x.UserId })
+            .IsUnique()
+            .HasFilter("\"UserId\" IS NOT NULL");
 
         builder.Property(x => x.Nama).HasMaxLength(200).IsRequired();
         builder.Property(x => x.Jabatan).HasMaxLength(100).IsRequired();
@@ -20,12 +22,17 @@ public class KaryawanConfiguration : IEntityTypeConfiguration<Karyawan>
         builder.Property(x => x.Email).HasMaxLength(200);
         builder.Property(x => x.Status).HasMaxLength(30);
         builder.Property(x => x.Catatan).HasMaxLength(1000);
-
+        builder.Property(x => x.PinHash).HasMaxLength(200);
         builder.Property(x => x.Gaji).HasPrecision(18, 2);
 
         builder.HasOne(x => x.Tenant)
             .WithMany(x => x.Karyawans)
             .HasForeignKey(x => x.TenantId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.User)
+            .WithOne()
+            .HasForeignKey<Karyawan>(x => x.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
