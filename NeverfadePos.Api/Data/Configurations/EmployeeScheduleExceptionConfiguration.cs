e4f41f576acad7ef2d1497d23598ca4de39666b9
Arkan <98953892+karkandea@users.noreply.github.com>
2026-09-02 10:22:30 +0700

    feat: configure schedule exceptions

---
+++ NeverfadePos.Api/Data/Configurations/EmployeeScheduleExceptionConfiguration.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NeverfadePos.Api.Entities;
+
+namespace NeverfadePos.Api.Data.Configurations;
+
+public sealed class EmployeeScheduleExceptionConfiguration : IEntityTypeConfiguration<EmployeeScheduleException>
+{
+    public void Configure(EntityTypeBuilder<EmployeeScheduleException> builder)
+    {
+        builder.ToTable("employee_schedule_exceptions", table =>
+            table.HasCheckConstraint(
+                "CK_employee_schedule_exceptions_Type",
+                "\"Type\" IN ('leave', 'holiday', 'changed_shift', 'off')"));
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => new { x.TenantId, x.KaryawanId, x.Date }).IsUnique();
+        builder.Property(x => x.Type).HasMaxLength(30).IsRequired();
+        builder.Property(x => x.Note).HasMaxLength(500);
+        builder.HasOne(x => x.Tenant)
+            .WithMany(x => x.EmployeeScheduleExceptions)
+            .HasForeignKey(x => x.TenantId)
+            .OnDelete(DeleteBehavior.Cascade);
+        builder.HasOne(x => x.Karyawan)
+            .WithMany(x => x.ScheduleExceptions)
+            .HasForeignKey(x => x.KaryawanId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
