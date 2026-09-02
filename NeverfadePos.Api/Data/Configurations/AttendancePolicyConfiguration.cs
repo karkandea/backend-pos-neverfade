3af9d08260cc4c8e62a43436c9eff98e52660131
Arkan <98953892+karkandea@users.noreply.github.com>
2026-09-02 10:22:14 +0700

    feat: configure attendance policy

---
+++ NeverfadePos.Api/Data/Configurations/AttendancePolicyConfiguration.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NeverfadePos.Api.Entities;
+
+namespace NeverfadePos.Api.Data.Configurations;
+
+public sealed class AttendancePolicyConfiguration : IEntityTypeConfiguration<AttendancePolicy>
+{
+    public void Configure(EntityTypeBuilder<AttendancePolicy> builder)
+    {
+        builder.ToTable("attendance_policies", table =>
+        {
+            table.HasCheckConstraint("CK_attendance_policies_GraceMinutes", "\"GraceMinutes\" BETWEEN 0 AND 180");
+            table.HasCheckConstraint("CK_attendance_policies_AbsenceThresholdMinutes", "\"AbsenceThresholdMinutes\" BETWEEN 1 AND 720");
+        });
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => x.TenantId).IsUnique();
+        builder.HasOne(x => x.Tenant)
+            .WithMany(x => x.AttendancePolicies)
+            .HasForeignKey(x => x.TenantId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
