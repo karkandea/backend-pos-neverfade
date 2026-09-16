ed6dfeda060eb21a3201509f2f8e08fa7db43e92
Arkan <98953892+karkandea@users.noreply.github.com>
2026-09-16 14:43:45 +0700

    feat: add outlet entity

---
+++ NeverfadePos.Api/Entities/Outlet.cs
@@ -0,0 +1,24 @@
+using NeverfadePos.Api.Common;
+
+namespace NeverfadePos.Api.Entities;
+
+public sealed class Outlet : BaseEntity
+{
+    public string Code { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Address { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
+
+    public bool IsDefault { get; set; }
+
+    public bool Active { get; set; } = true;
+
+    public Tenant? Tenant { get; set; }
+
+    public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+    public ICollection<WhatsAppSender> WhatsAppSenders { get; set; } = new List<WhatsAppSender>();
+}
